7e50e0292005ff537b26ed716597fef273a8b4e5
Rajmohan <raajmohanrr@gmail.com>
2022-02-08 17:34:06 +0530

    folder structure created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Container} from "react-bootstrap";
 
-function App() {
+import NavigationBar from "./components/NavigationBar";
+import OurVision from "./components/OurVision";
+import Features from "./components/Features";
+import OurTeam from "./components/OurTeam";
+import LatestNews from "./components/LatestNews";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <NavigationBar/>
+        <OurVision/>
+        <Features/>
+        <OurTeam/>
+        <LatestNews/>
+        <Contact/>
+        <Footer/>
+      </Container>
     </div>
   );
 }
-
-export default App;
